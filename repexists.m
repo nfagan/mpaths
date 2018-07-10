14b38b08246ef1a6e45867fb5b947dfa12219702
nfagan <nick@fagan.org>
2018-07-10 14:18:57 -0400

    update help text

--- repexists.m
+++ repexists.m
@@ -3,7 +3,7 @@ function tf = repexists(name)
 %   REPEXISTS -- True if repository exists.
 %
 %     repexists( 'example' ) returns true if 'example' is a repository
-%     housed in the folder given by `repdir`, as defined by `repdef`.
+%     housed in the folder given by `repdir()`, as defined by `repdef()`.
 %
 %     See also repget, repdir, repdef
 %
