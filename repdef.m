f080a16c5a865181a7f66fcf3a484048f9d02553
nfagan <nick@fagan.org>
2018-07-27 11:06:57 -0400

    add default

--- repdef.m
+++ repdef.m
@@ -6,6 +6,9 @@ function repdef(p)
 %     folder housing this function ('repdef.m'). The file contains the
 %     string '~/Users/Repositories'.
 %
+%     repdef( '--default' ) or repdef --default defines the repositories
+%     directory as the folder containing the `mpaths` repository.
+%
 %     See also repdir, repfname
 %
 %     IN:
@@ -13,8 +16,37 @@ function repdef(p)
 
 assert( ischar(p), 'Path must be char; was "%s".', class(p) );
 
+if ( strcmpi(p, '--default') )
+  p = get_default_repo_dir();
+end
+
 save( repfname(), 'p' );
 
 fprintf( '\nSaved "%s" as repositories folder\n\n', p );
+
+end
+
+function p = get_default_repo_dir()
+
+parts = fileparts( which(mfilename) );
+slash = get_slash();
+
+split = strsplit( parts, slash );
+
+if ( numel(split) < 2 || any(cellfun(@isempty, split)) )
+  error( 'Could not locate containing directory for directory "%s".', parts );
+end
+
+p = fullfile( split{1:end-1} );
+
+end
+
+function slash = get_slash()
+
+if ( ispc() )
+  slash = '\';
+else
+  slash = '/';
+end
 
 end
