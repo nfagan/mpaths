fb56beb16587980b7574dd5ab7b32d52451ded36
nfagan <nick@fagan.org>
2018-12-05 11:26:26 -0500

    fix --default flag in repdef(); update help text

--- repdef.m
+++ repdef.m
@@ -1,23 +1,45 @@
-function repdef(p)
+function p = repdef(p, allow_non_existing)
 
 %   REPDEF -- Define path to repositories directory.
 %
-%     repdef( '~/Users/Repositories' ) saves a file 'mpaths.mat' in the
-%     folder housing this function ('repdef.m'). The file contains the
-%     string '~/Users/Repositories'.
+%     repdef( directory ) defines the absolute path to a repositories 
+%     `directory` (that is, a folder containing subfolders of code).
+%
+%     Subsequently, the `repadd` command (as in, `repadd(subfoldername)`)
+%     can be used to add a subfolder of `directory` to Matlab's search
+%     path.
 %
 %     repdef( '--default' ) or repdef --default defines the repositories
 %     directory as the folder containing the `mpaths` repository.
 %
-%     See also repdir, repfname
+%     repdef( ..., allow_non_existing ) indicates whether a non-existing
+%     directory can be saved. Default is false -- an error is thrown if 
+%     `directory` does not exist.
+%
+%     p = repdef(...) returns the saved directory path.
+%
+%     See also repdir, repfname, repadd
 %
 %     IN:
 %       - `p` (char)
+%     OUT:
+%       - `p` (char)
 
-assert( ischar(p), 'Path must be char; was "%s".', class(p) );
+if ( nargin < 2 )
+  allow_non_existing = false;
+end
+
+validateattributes( p, {'char'}, {'scalartext'}, mfilename, 'p' );
+validateattributes( allow_non_existing, {'logical'}, {'scalar'}, mfilename ...
+  , 'allow_non_existing' );
 
 if ( strcmpi(p, '--default') )
   p = get_default_repo_dir();
+end
+
+if ( ~allow_non_existing )
+  assert( exist(p, 'dir') == 7, ['Directory "%s" does not exist, and' ...
+    , ' "allow_non_existing" flag is false.'], p );
 end
 
 save( repfname(), 'p' );
@@ -32,12 +54,17 @@ parts = fileparts( which(mfilename) );
 slash = get_slash();
 
 split = strsplit( parts, slash );
+split(cellfun(@isempty, split)) = [];
 
-if ( numel(split) < 2 || any(cellfun(@isempty, split)) )
+if ( numel(split) < 2 )
   error( 'Could not locate containing directory for directory "%s".', parts );
 end
 
 p = fullfile( split{1:end-1} );
+
+if ( ~isempty(p) && p(1) ~= slash )
+  p = [ slash, p ];
+end
 
 end
 
